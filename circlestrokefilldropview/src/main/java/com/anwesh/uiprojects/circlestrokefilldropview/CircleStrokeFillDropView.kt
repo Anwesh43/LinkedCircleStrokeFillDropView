798f2e7e058a681b82e0055bbe88aca056822173
Anwesh43 <anweshthecool0@gmail.com>
2020-06-01 08:38:16 +0530

    adding CircleStrokeFillDropView class

--- circlestrokefilldropview/src/main/java/com/anwesh/uiprojects/circlestrokefilldropview/CircleStrokeFillDropView.kt
+++ circlestrokefilldropview/src/main/java/com/anwesh/uiprojects/circlestrokefilldropview/CircleStrokeFillDropView.kt
@@ -49,3 +49,19 @@ fun Canvas.drawSFDNode(i : Int, scale : Float, paint : Paint) {
     drawStrokeFillDropCircle(scale, w, h, paint)
     restore()
 }
+
+class CircleStrokeFillDropView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
