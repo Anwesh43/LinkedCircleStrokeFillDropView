e4d7c71963584de7310a30e3c44e915c7358d515
Anwesh43 <anweshthecool0@gmail.com>
2020-06-01 08:59:29 +0530

    adding CircleStrokeFillDrop class

--- circlestrokefilldropview/src/main/java/com/anwesh/uiprojects/circlestrokefilldropview/CircleStrokeFillDropView.kt
+++ circlestrokefilldropview/src/main/java/com/anwesh/uiprojects/circlestrokefilldropview/CircleStrokeFillDropView.kt
@@ -138,6 +138,10 @@ class CircleStrokeFillDropView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
         fun getNext(dir : Int, cb : () -> Unit) : CSFDNode {
             var curr : CSFDNode? = prev
             if (dir == 1) {
@@ -148,6 +152,29 @@ class CircleStrokeFillDropView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class CircleStrokeFillDrop(var i : Int) {
+
+        private var curr : CSFDNode = CSFDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
