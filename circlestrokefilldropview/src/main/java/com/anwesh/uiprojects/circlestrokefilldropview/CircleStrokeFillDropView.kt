640a67ddee84cbc608a6b0b77f970c13614a0b8a
Anwesh43 <anweshthecool0@gmail.com>
2020-06-01 08:56:37 +0530

    adding CSFDNOde class

--- circlestrokefilldropview/src/main/java/com/anwesh/uiprojects/circlestrokefilldropview/CircleStrokeFillDropView.kt
+++ circlestrokefilldropview/src/main/java/com/anwesh/uiprojects/circlestrokefilldropview/CircleStrokeFillDropView.kt
@@ -112,4 +112,42 @@ class CircleStrokeFillDropView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CSFDNode(var i : Int, val state : State = State()) {
+
+        private var next : CSFDNode? = null
+        private var prev : CSFDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CSFDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSFDNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CSFDNode {
+            var curr : CSFDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
